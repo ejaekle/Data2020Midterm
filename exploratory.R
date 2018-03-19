4e94c431bc84632b8a862be448e8aa844b72fb3f
Emily Jaekle <emily_jaekle@brown.edu>
2018-03-19 17:14:53 -0400

    third commit

--- exploratory.R
+++ exploratory.R
@@ -34,7 +34,51 @@ nyc_census <- census4
 # so realistically we lost less than 3% 
 
 summary(nyc_census)
-pairs(nyc_census[1:10,])
+#pairs(nyc_census[1:10,])
+
+# all the data 
+
+names(nyc_census)
+hist(nyc_census$Income) # trying to predict 
+hist(nyc_census$IncomePerCap) # also trying to predict 
+
+
+# split up data into boroughs 
+
+brooklyn <- nyc_census[(nyc_census$Borough=='Brooklyn'),]
+manhattan <- nyc_census[(nyc_census$Borough=='Manhattan'),]
+queens <- nyc_census[(nyc_census$Borough=='Queens'),]
+bronx <- nyc_census[(nyc_census$Borough=='Bronx'),]
+staten.island <- nyc_census[(nyc_census$Borough=='Staten Island'),]
+
+sum(brooklyn$TotalPop)
+sum(manhattan$TotalPop)
+sum(queens$TotalPop)
+sum(bronx$TotalPop)
+sum(staten.island$TotalPop)
+
+# brooklyn, queen, manhattan, bronx, staten island 
+
+#total hispanics in brooklyn approx 
+percentages <- (brooklyn$Hispanic)/100
+sum(percentages*brooklyn$TotalPop)
+
+percentages <- (manhattan$Hispanic)/100
+sum(percentages*manhattan$TotalPop)
+
+percentages <- (queens$Hispanic)/100
+sum(percentages*queens$TotalPop)
+
+percentages <- (bronx$Hispanic)/100
+sum(percentages*bronx$TotalPop)
+
+percentages <- (staten.island$Hispanic)/100
+sum(percentages*staten.island$TotalPop)
+
+#can do this with white black native asian columns......
+
+
+
 
 
 
